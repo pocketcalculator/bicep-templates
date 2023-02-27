eb49f14c9134b42717d322838afd5393f369f71a
Paul Sczurek <pocketcalculatorshow@gmail.com>
2023-02-27 14:04:06 -0500

    unset bash vars

--- static-web-app/staticWebApp-azure-bicep.sh
+++ static-web-app/staticWebApp-azure-bicep.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-subscription=null
-location=eastus2
-application=
-repositoryUrl=
-repositoryBranch=
-appLocation=build
-repositoryToken=
-environment=production
-owner=pocketcalculatorshow@gmail.com
+location=<region>
+application=<application_name>
+repositoryUrl=<github_repository_url>
+repositoryBranch=<git_branch>
+appLocation=<content_directory>
+repositoryToken=<github_personal_access_token>
+environment=<environment>
+owner=<owner_email_address>
 resourceGroupName=rg-$application-$environment-$location
 skuName=Standard
 skuTier=Standard
 
-echo subscription = $subscription
 echo location = $location
 echo application = $application
 echo repositoryUrl = $repositoryUrl
